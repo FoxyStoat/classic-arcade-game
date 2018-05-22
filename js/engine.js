f521c84d27e5b2f709f80ac65a1d86badcf6a890
FoxyStoat <carol.hodgson@hotmail.com>
2018-05-22 15:38:38 +0100

    render the gem on screen

--- js/engine.js
+++ js/engine.js
@@ -154,6 +154,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to
